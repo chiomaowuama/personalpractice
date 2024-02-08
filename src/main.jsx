c545ee1b90c23285d2e5bf47cad9755bf2798eaa
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-08 22:23:14 +0100

    trying to help its show

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Layout from './Layout.jsx'
+import Home from './component/Home/Home.jsx'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home/>}/>
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
