efc774f2f5c0819e72b933c1753b357fbe327937
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-09 19:00:58 +0100

    trying io put it in place

--- src/context/ThemeContextProvider.jsx
+++ src/context/ThemeContextProvider.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeContext } from "./ThemeContext";
 
-export const ThemeProvider = theme.Provider
+export const ThemeProvider = ({children}) =>{
+    const[ Theme, setTheme] = useState(' light')
 
-export default function usetheme(){
-    return(useContext(theme))
+    const darktheme = () => {
+        setTheme('dark')
+    }
+    
+    const  lighttheme = () => {
+        setTheme('light')
+      }
+    
+      useEffect(() => {
+        document.querySelector('html').classList.remove('dark', "light")
+        document.querySelector('html').classList.add(Theme)
+      }, [Theme])
+    
+      return (
+        <ThemeContext.Provider value={{Theme, darktheme, lighttheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+
 }
