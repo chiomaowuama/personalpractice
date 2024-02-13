b511761875f7828f512bb3b16fa19f8624b15b9a
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-13 01:53:59 +0100

    hving error on the fullname

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Theme from '../Themes/Theme'
 import  ThemeContext  from '../../context/ThemeContext'
 import { useContext } from 'react'
@@ -6,10 +6,45 @@ import { useContext } from 'react'
 
 function Home() {
   const { Theme, darktheme, lighttheme} = useContext( ThemeContext)
+  const [FirstName, setFirstName]= useState('')
+  const [SecondName, setSecondName]= useState('')
+  const [Fullname, setFullname ]= useState(' ')
+
+  console.log(FirstName)
+  console.log(SecondName)
+  // console.log(Fullname)
+
+  const Getname = (e)=>{
+    e.preventdefault()
+      const Fullname = FirstName 
+      console.log(Fullname)
+  }
   return (
     <div>
-      <h1 className='dark:bg-green-800'>hello</h1>
       
+      <div>
+        <form onSubmit={(e) => Getname} className='border-2 border-green-900 dark:bg-green-900'>
+          <div className='border-2 border-yellow-900 block '>
+            <label htmlFor="">FirstName</label>
+            <input type="text" 
+            placeholder='Fristname'
+            className='border-2 border-green-900'
+            onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className='block'>
+            <label htmlFor="">SecondName</label>
+            <input type="text" 
+            placeholder='Fristname'
+            className='border-2 border-green-900'
+            onChange={(e) => setSecondName(e.target.value)}
+            />
+          </div>
+          
+          <button  className='border-2 border-black'>Get FullName</button>
+        </form>
+        <p > fullname: {}</p>
+      </div>      
     </div>
   )
 }
