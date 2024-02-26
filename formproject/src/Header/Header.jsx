3247439869319274f6ab90ceddb4b05c96d2921d
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-26 14:54:16 +0100

    struggling with navlink

--- formproject/src/Header/Header.jsx
+++ formproject/src/Header/Header.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    <div className='border-2 p-2 flex flex-row '>
+        <div className='w-1/6 border-2 border-black'>
+            <img src="" alt="" />
+            <h1>hello</h1>
+        </div>
+        <div className='w-4/6 border-2 border-green-800'>
+            <div className='w-4/5 mx-auto'>
+            <ul className='flex text-blue-950 text-lg font-mono font-semibold justify-around items-start '>
+
+                <li className=''>
+                    <NavLink
+                        to="/"
+                            className={({isActive}) =>
+                                `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
+                                ${isActive ? "text-orange-700" : "text-gray-700" } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            Home
+                    </NavLink>
+                </li>
+                <li  className=''>
+                    <NavLink 
+                    to="/Contactus"
+                            className={({isActive}) =>
+                            `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
+                            ${isActive ? "text-orange-700" : "text-gray-700"} lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}
+                  
+                    >
+
+                    Contact
+                    </NavLink>
+                </li>
+                <li  className=''>Register</li>
+                <li  className=''>Blogs</li>
+            </ul>
+            </div>
+        </div>
+        <div className='w-1/6 border-2 border-black flex justify-center'>
+            <p>cart</p>
+        </div>
+    </div>
+    </>
   )
 }
 
--- formproject/src/main.jsx
+++ formproject/src/main.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
+import Home from './Home/Home.jsx'
+import About from './About/About.jsx'
+import Contactus from './Contactus/Contactus.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>} >
-
-      
-    </Route>
+    <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home/>}/>
+        <Route path='' element={<About/>}/>
+        <Route path='/Contactus' element={<Contactus/>}/>
+      </Route>
   )
 )
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
