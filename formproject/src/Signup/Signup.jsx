9de1cbc360649753a5a6579c87a8514495e7be73
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-27 13:17:17 +0100

    the form all set up

--- formproject/src/Signup/Signup.jsx
+++ formproject/src/Signup/Signup.jsx
@@ -4,10 +4,18 @@ function Signup() {
   return (
     <>
     <div>
-        <form action="">
-            <div className='flex flex-col' >
-            <label htmlFor=''>Username</label>
-            <input type='text' className=' border border-black'></input>
+        <form action="" className='flex flex-col justify-center items-center border-2 border-yellow-900 h-screen '>
+            <div className='flex flex-col w-4/6  space-y-2  border-2 border-green-900 shadow-xl shadow-slate-400 p-3' >
+                <p>Form Submission</p>
+                <label htmlFor=''>Username</label>
+                <input type='text' className=' border border-black'/>
+                <label htmlFor="password">Password</label>
+                <input type="text" name="" id="" className=' border border-black' />
+                <label htmlFor="email">Email</label>
+                <input type="email" name="" id=""  className=' border border-black'/>
+                <label htmlFor="Phone">Phone No</label>
+                <input type="tel" name="" min={10} max={11} id="" className=' border border-black'/>
+            <button type="submit" className='border-2 border-green-700 w-3/5 self-center'>Submit</button>
             </div>
         </form>
        
