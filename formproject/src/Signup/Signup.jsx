ba7153d343b8aa994e4e41c91d40128ebc9638cb
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-27 13:41:06 +0100

    i added dev tools

--- formproject/src/Signup/Signup.jsx
+++ formproject/src/Signup/Signup.jsx
@@ -11,13 +11,13 @@ function Signup() {
             <div className='flex flex-col w-4/6  space-y-2  border-2 border-green-900 shadow-xl shadow-slate-400 p-3' >
                 <p>Form Submission</p>
                 <label htmlFor=''>Username</label>
-                <input type='text' className=' border border-black'/>
+                <input type='text' id='text' {...register("username")} className=' border border-black'/>
                 <label htmlFor="password">Password</label>
-                <input type="text" name="" id="" className=' border border-black' />
+                <input type="text" name="" id="password"  {...register("password")} className=' border border-black' />
                 <label htmlFor="email">Email</label>
-                <input type="email" name="" id=""  className=' border border-black'/>
+                <input type="email" name="" id="email"  {...register("email")}  className=' border border-black'/>
                 <label htmlFor="Phone">Phone No</label>
-                <input type="tel" name="" min={10} max={11} id="" className=' border border-black'/>
+                <input type="tel" name="" min={10} max={11} id="phone"  {...register("phone")} className=' border border-black'/>
             <button type="submit" className='border-2 border-green-700 w-3/5 self-center'>Submit</button>
             </div>
         </form>
