42cfe2a58f5f8a0a8c7b6fb61bd0c099c2b73350
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-02-27 12:56:42 +0100

    frist commit of the day

--- formproject/src/Signup/Signup.jsx
+++ formproject/src/Signup/Signup.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 function Signup() {
   return (
     <>
+    <div>
+        <form action="">
+            <div className='flex flex-col' >
+            <label htmlFor=''>Username</label>
+            <input type='text' className=' border border-black'></input>
+            </div>
+        </form>
+       
+    </div>
     
     
     </>
