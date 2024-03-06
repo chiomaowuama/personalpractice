1bdc69783c02cdf51682ef6d9a4656aff3720a95
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-06 11:15:40 +0100

    made a simple form

--- formproject/src/AnotherSignup/Signuptwo.jsx
+++ formproject/src/AnotherSignup/Signuptwo.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function Signuptwo() {
   return (
-    <div>Signuptwo</div>
+    <>
+    <div className='border-2 border-green-400 h-lvh flex justify-center'>
+
+       <form action="" className='w-3/5  border-2  self-center '>
+            <h1 className='text-2xl text-wrap font-serif font-semibold uppercase text-center text-red-400 '>my own form</h1>
+            <div className='flex flex-col space-y-2 border-2'>
+                <label htmlFor="">FirstName</label>
+                <input type="text" className='border border-dashed border-gray-400 w-2/5' />
+            </div>
+            <div className='flex flex-col space-y-2 border-2'>
+                <label htmlFor="">SecondName</label>
+                <input type="text" className='border border-dashed border-gray-400 w-2/5' />
+            </div>
+            <div className='flex flex-col space-y-2 border-2'>
+                <label htmlFor="">Email</label>
+                <input type="email" className='border border-dashed border-gray-400 w-2/5' />
+            </div>
+            <div className='flex flex-col space-y-2 border-2'>
+                <label htmlFor="">PhoneNo</label>
+                <input type="tel" className='border border-dashed border-gray-400 w-2/5' />
+            </div>
+       
+       </form>
+    </div>
+    </>
   )
 }
 
