217aa60675875f6ada04c8a9c37a6b740b0d345e
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-06 11:41:52 +0100

    form almost completed

--- formproject/src/AnotherSignup/Signuptwo.jsx
+++ formproject/src/AnotherSignup/Signuptwo.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function Signuptwo() {
+    const form = useForm()
+    const {register} = form
+   
   return (
     <>
-    <div className='border-2 border-green-400 h-lvh flex justify-center'>
+    <div className='allbg  h-lvh flex justify-center'>
 
-       <form action="" className='w-3/5  border-2  self-center '>
+       <form action="" className=' px-2 pb-8 bg-green-800 w-3/6 self-center text-white'>
             <h1 className='text-2xl text-wrap font-serif font-semibold uppercase text-center text-red-400 '>my own form</h1>
-            <div className='flex flex-col space-y-2 border-2'>
+            <div className='flex flex-col space-y-3 mb-2'>
                 <label htmlFor="">FirstName</label>
-                <input type="text" className='border border-dashed border-gray-400 w-2/5' />
+                <input type="text" id="firstname" {...register('firstname')} className='border border-dashed border-gray-400 w-2/5' />
             </div>
-            <div className='flex flex-col space-y-2 border-2'>
+            <div className='flex flex-col space-y-3  mb-2'>
                 <label htmlFor="">SecondName</label>
-                <input type="text" className='border border-dashed border-gray-400 w-2/5' />
+                <input type="text" id="Secondname" {...register('secondname')}  className='border border-dashed border-gray-400 w-2/5' />
             </div>
-            <div className='flex flex-col space-y-2 border-2'>
+            <div className='flex flex-col space-y-2  mb-2'>
                 <label htmlFor="">Email</label>
-                <input type="email" className='border border-dashed border-gray-400 w-2/5' />
+                <input type="email" id="Email" {...register('email')} className='border border-dashed border-gray-400 w-2/5' />
             </div>
-            <div className='flex flex-col space-y-2 border-2'>
+            <div className='flex flex-col space-y-2  mb-2'>
                 <label htmlFor="">PhoneNo</label>
-                <input type="tel" className='border border-dashed border-gray-400 w-2/5' />
+                <input type="tel" id="Email" {...register('email')} className='border border-dashed border-gray-400 w-2/5' />
             </div>
        
        </form>
--- formproject/src/Header/Header.jsx
+++ formproject/src/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
 
     <div>
       {/* the second nav */}
-      <header className="shadow sticky z-50 top-0">
+      <header className="headers shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 ">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
