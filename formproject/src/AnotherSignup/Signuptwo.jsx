5f25ddb757c7df7733f1be9f24c842dff1165587
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-07 09:45:48 +0100

    first commit of the day

--- formproject/src/AnotherSignup/Signuptwo.jsx
+++ formproject/src/AnotherSignup/Signuptwo.jsx
@@ -1,35 +1,57 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { DevTool} from '@hookform/devtools'
 
 function Signuptwo() {
     const form = useForm()
-    const {register} = form
+    const {register, control,handleSubmit, formState} = form
+    const {errors} = formState
+
+    const alldetails = (data) =>{
+        // e.preventDefault()
+        console.log('helo', data)
+    }
    
   return (
     <>
     <div className='allbg  h-lvh flex justify-center'>
 
-       <form action="" className=' px-2 pb-8 bg-green-800 w-3/6 self-center text-white'>
-            <h1 className='text-2xl text-wrap font-serif font-semibold uppercase text-center text-red-400 '>my own form</h1>
+       <form action="" noValidate onClick={handleSubmit(alldetails)}  className=' px-2 pb-8 bg-green-800 w-4/6 lg:w-3/6 self-center text-white'>
+            <h1 className='text-2xl text-wrap font-serif font-semibold uppercase text-center text-white py-3'>my own form</h1>
             <div className='flex flex-col space-y-3 mb-2'>
                 <label htmlFor="">FirstName</label>
-                <input type="text" id="firstname" {...register('firstname')} className='border border-dashed border-gray-400 w-2/5' />
-            </div>
+                <input type="text" id="firstname" {...register('firstname', {required:"firstname is required"})} className=' text-black border border-dashed border-gray-400 w-4/5 lg:w-2/5' />
+              
+            </div> 
             <div className='flex flex-col space-y-3  mb-2'>
                 <label htmlFor="">SecondName</label>
-                <input type="text" id="Secondname" {...register('secondname')}  className='border border-dashed border-gray-400 w-2/5' />
+ 
+                <input type="text"  {...register('secondname', {required:"secondname is required"})}   className=' text-black border  w-4/5 lg:w-2/5'/>
+
             </div>
             <div className='flex flex-col space-y-2  mb-2'>
                 <label htmlFor="">Email</label>
-                <input type="email" id="Email" {...register('email')} className='border border-dashed border-gray-400 w-2/5' />
+                <input type="email" id="Email" {...register('email', {
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                        message: "Invalid email format",
+                      },
+                })} className=' text-black border border-dashed border-gray-400 w-4/5 lg:w-2/5' />
             </div>
+           
             <div className='flex flex-col space-y-2  mb-2'>
                 <label htmlFor="">PhoneNo</label>
-                <input type="tel" id="Email" {...register('email')} className='border border-dashed border-gray-400 w-2/5' />
+                <input type="tel" id="tell" {...register('phone', {required:"phonenumber is required"})} className=' text-black border border-dashed border-gray-400 w-4/5 lg:w-2/5' />
+            </div>
+            <div>
+                <button type="button" className='border-white border-2 bg-green-800  my-2 rounded-md w-4/5 lg:w-2/5'> submit</button>
             </div>
        
        </form>
+       
     </div>
+    <DevTool control={control} />
     </>
   )
 }
